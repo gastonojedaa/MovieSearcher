477b2a83ec5df142297aa9eb47e53c7a5cd94789
gastonojedaa <gastonojeda@mi.unc.edu.ar>
2024-05-30 14:59:56 -0300

    style: some changes on the grid cards

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,10 +4,17 @@ export function responseMovies({ movies }) {
 		<>
 			<ul className='grid grid-cols-3 gap-4 mt-10'>
 				{movies.map((movie) => (
-					<li key={movie.Title} className='movie'>
+					<li key={movie.Title} className='movie relative group rounded-xl'>
 						<h2>{movie.Title}</h2>
-						
 						<img src={movie.Poster} alt={movie.Title} />
+						<p>{movie.Year}</p>
+						<div className='absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-75 opacity-0 group-hover:opacity-100 transition-opacity rounded-xl'>
+							<a href='http://www.youtube.com' target="_blank" rel="noopener noreferrer">
+								<button className='px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 transition'>
+									Watch Trailer
+								</button>
+							</a>
+						</div>
 					</li>
 				))}
 			</ul>
